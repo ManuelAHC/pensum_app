bcd6a9f0d223f5da2bb806ac42641468d7edc9cf
ManuelAHC <manuelalex0110@gmail.com>
2025-09-15 14:10:04 -0400

    Registering avance model in admin

--- avance/admin.py
+++ avance/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Avance
 
-# Register your models here.
+@admin.register(Avance)
+class AvanceAdmin(admin.ModelAdmin):
+    list_display = ('estudiante', 'materia', 'aprobado', 'fecha_aprobacion')
+    list_filter = ('aprobado', 'materia', 'estudiante')
+    search_fields = ('estudiante__user__username', 'materia__nombre')
