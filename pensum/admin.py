5544f858bed59b674513d486449cfdaabd1f5272
ManuelAHC <manuelalex0110@gmail.com>
2025-09-15 19:37:50 -0400

    Registering Pensum model in admin

--- pensum/admin.py
+++ pensum/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Pensum
+
+@admin.register(Pensum)
+class PensumAdmin(admin.ModelAdmin):
+    list_display = ('carrera', 'materia', 'semestre', 'es_electiva')
+    list_filter = ('carrera', 'semestre', 'es_electiva')
+    search_fields = ('carrera__nombre', 'materia__nombre')
